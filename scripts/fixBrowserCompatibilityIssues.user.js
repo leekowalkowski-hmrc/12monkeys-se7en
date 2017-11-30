3a0d2d803800de269f2e24e28367af10e983220a
Lee Kowalkowski <lee.kowalkowski@digital.hmrc.gov.uk>
2017-11-30 16:19:56 +0000

    Put dropdown in front of table header

--- scripts/fixBrowserCompatibilityIssues.user.js
+++ scripts/fixBrowserCompatibilityIssues.user.js
@@ -1,17 +1,22 @@
 // ==UserScript==
 // @name         Browser Compatibility
 // @namespace    12monkeys-se7en
-// @version      0.2
+// @version      0.3
 // @description  Fix browser compatibility issues
 // @author       Lee Kowalkowski, Ian Ralphs
 // @match        https://*/*/DTX.NET/*
 // @updateURL    https://raw.githubusercontent.com/leekowalkowski-hmrc/12monkeys-se7en/master/scripts/fixBrowserCompatibilityIssues.user.js
 // @downloadURL  https://raw.githubusercontent.com/leekowalkowski-hmrc/12monkeys-se7en/master/scripts/fixBrowserCompatibilityIssues.user.js
-// @grant        none
+// @resource     CSS https://raw.githubusercontent.com/leekowalkowski-hmrc/12monkeys-se7en/master/styles/fix.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    var cssTxt = GM_getResourceText ("CSS");
+    GM_addStyle(cssTxt);
 
     document.querySelectorAll("[onpropertychange]").forEach(element => {
         element.setAttribute('onchange', element.getAttribute('onpropertychange'));
